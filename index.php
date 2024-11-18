b99ef858390b87d7c8a4bcc8e9dd6582d97690bf
NavridaRully <navrida.rully@gmail.com>
2024-11-18 13:55:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$NIM = "312310328";
+$Nama = "Navrida Rully Anastasya";
+$Kelas = "TI.23.CA.1";
+
+?>
+
+<h1>
+ <strong>Nama</strong>=<?php echo $Nama?>
+</h2>
+<h2>
+ <strong>Kelas</strong>=<?php echo $Kelas?>
+</h2>
+<h2>
+ <strong>NIM</strong>=<?php echo $NIM?>
+</h2>
